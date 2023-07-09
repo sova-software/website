06967da0e55fb24b7c64268bf7dbb570b0de9768
tizu69 <60812901+tizu69@users.noreply.github.com>
2023-07-09 21:27:41 +0200

    OMG MITKO????

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="text-gray-400 text-center">
 	&copy; Sova 2023<br />
 	Contact us @ support[at]sovasoftware.net<br />
-	<a href="/license" class="anchor">License</a> &bull;
+	<a href="/license" class="anchor">License</a> &bull; Design by <a href="https://github.com/tizu69" class="anchor">tizu69</a>
 </footer>
