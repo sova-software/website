b3914c7b59f07a023a35fcebe07a4f2c4b720087
tizu69 <60812901+tizu69@users.noreply.github.com>
2023-07-09 20:19:37 +0200

    Merged NavBars

---
+++ src/lib/components/navbar/NavBar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	//  -> Imports
+	import { RadioGroup, RadioItem, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
+
+	//  -> Local imports
+	import { scrollStore } from '$lib/stores';
+	import { page } from '$app/stores';
+</script>
+
+<nav
+	class="nav"
+>
+	<a href="/" class="w-20">
+		<img src="/sova.svg" alt="Sova logo" />
+	</a>
+	<TabGroup
+		active="variant-soft-primary"
+		rounded="rounded-token"
+		space="space-x-1"
+		class="bg-surface-200-700-token border-token border-surface-400-500-token rounded-token p-1"
+		border=""
+	>
+		<TabAnchor href="/" selected={$page.url.pathname === '/'}>Home</TabAnchor>
+		<TabAnchor href="/projects" selected={$page.url.pathname === '/projects'}>Projects</TabAnchor>
+		<TabAnchor href="/about" selected={$page.url.pathname === '/about'}>About</TabAnchor>
+	</TabGroup>
+</nav>
