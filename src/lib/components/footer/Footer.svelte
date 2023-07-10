27e2616851a1e32b0435c446435cf78583f6280b
javv <80885806+javtheslav@users.noreply.github.com>
2023-07-10 20:59:40 +0200

    footer co+ntact clickable + italic for coolness :sunglasses:

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="text-gray-400 text-center">
 	&copy; Sova 2023<br />
-	Contact us @ support[at]sovasoftware.net<br />
+	Contact us @ <a href="mailto:support@sovasoftware.net"><i>support@sovasoftware.net</i><br />
 	<a href="/license" class="anchor">License</a> &bull; Design by
 	<a href="https://github.com/tizu69" class="anchor">tizu69</a>
 </footer>
