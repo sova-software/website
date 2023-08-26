ec7a429cb1770a03b862feb20cd9c5a3b33987e1
Mitko <admin@sovasoftware.net>
2023-08-26 19:27:14 +0300

    @

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="text-gray-400 text-center">
 	&copy; Sova 2023<br />
-	Contact us @ support@sovasoftware.net<br />
+	Contact us at support@sovasoftware.net<br />
 	<a href="/license" class="anchor">License</a> &bull; Design by
 	<a href="https://github.com/tizu69" class="anchor">tizu69</a>
 </footer>
